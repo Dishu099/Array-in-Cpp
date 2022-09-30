23248de6d8c2fbb64c3366c6ce4669163cf89022
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-09-30 18:46:17 +0530

    Create Index of first 1 in a sorted array of 0s and 1s.cpp

---
+++ Index of first 1 in a sorted array of 0s and 1s.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstIndex(int a[], int n) 
+    {
+        // Your code goes here
+        for(int i=0;i<n;i++){
+            if(a[i]==1)
+                return i;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.firstIndex(a, n) << endl;
+    }
+}
+// } Driver Code Ends
