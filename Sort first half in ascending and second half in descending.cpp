10052ce226895007f3baefcd2d1b2251b19a7d99
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-03 17:55:14 +0530

    Create Sort first half in ascending and second half in descending.cpp

---
+++ Sort first half in ascending and second half in descending.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution{
+  public:
+    void customSort(int arr[], int n) {
+        // code here 
+        int k=n/2;
+  sort(arr, arr + k);
+  sort(arr + k, arr + n, greater<int>()); 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        obj.customSort(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
