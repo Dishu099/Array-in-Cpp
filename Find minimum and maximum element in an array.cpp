b3f7796ced3ada9539909389e90e74217a6daf7e
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-25 11:06:55 +0530

    Create Find minimum and maximum element in an array.cpp

---
+++ Find minimum and maximum element in an array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    long long mini=INT_MAX, maxi=INT_MIN;
+    pair<long long,long long> p;
+     for(int i=0;i<n;i++){
+
+        mini=min(a[i],mini);
+
+
+        maxi=max(a[i],maxi);
+
+  }
+    p={mini,maxi};
+    return p;
+}
