a0144b863f0c0a9bb12512d0d0439e9f92c65c93
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-30 15:32:56 +0530

    Create Largest Element in Array.cpp

---
+++ Largest Element in Array.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int max = arr[0]; 
+	for (int i = 1; i < n; i++) 
+		if (arr[i] > max) 
+			max = arr[i]; 
+
+	return max; 
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
