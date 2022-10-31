9509ef016002b221bef7ac08ba5cb6a6c08da331
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-31 16:50:27 +0530

    Create Smallest Positive missing number.cpp

---
+++ Smallest Positive missing number.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        sort(arr, arr+n);
+    int smallest = 1; 
+    for(int i =0; i< n; i++)
+    {
+        if(arr[i] == smallest)
+            smallest ++; 
+    }
+
+    return smallest; 
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.miss
