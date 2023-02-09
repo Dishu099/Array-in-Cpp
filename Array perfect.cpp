8d5e1e12120670b7b1dfbda9c53f11aac5c84e4c
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2023-02-09 23:15:12 +0530

    Create Array perfect.cpp

---
+++ Array perfect.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int check(int arr[], int n)
+    {
+        // Complete the function
+         int i = 1;
+    while (arr[i] > arr[i - 1] && i < n)
+        ++i;
+    while (arr[i] == arr[i - 1] && i < n)
+        ++i;
+    while (arr[i] < arr[i - 1] && i < n)
+        ++i;
+    return (i == n);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0; i<n; i++)
+           cin>>arr[i];
+        Solution ob;
+        if (ob.check(arr, n))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+}
