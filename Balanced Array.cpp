dd89fa2f1ba1a02b3f1df4a751769c90db847b52
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2023-03-08 16:36:58 +0530

    Create Balanced Array.cpp

---
+++ Balanced Array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int minValueToBalance(int a[], int n)
+    {
+       //code here.
+       int x=0;
+       for (int i = 0; i < n/2; i++){
+           x+=a[i];
+       }
+       int y=0;
+       for(int i=n/2;i<n;i++){
+           y+=a[i];
+       }
+       return abs(x-y);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;	
+		cout<<ob.minValueToBalance(a,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
